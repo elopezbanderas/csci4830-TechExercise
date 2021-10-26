112cbb061241d551b03ca25092ce8567c21d2356
eloylopez <eloylopez@192.168.0.22>
2021-10-25 19:02:44 -0500

    Error handling in log in for invalid nuid

--- csci4830-TechExercise/src/util/UtilDBLopezBanderas.java
+++ csci4830-TechExercise/src/util/UtilDBLopezBanderas.java
@@ -22,25 +22,26 @@ import org.hibernate.Transaction;
  * @since JavaSE-1.8
  */
 public class UtilDBLopezBanderas {
-   static SessionFactory sessionFactory = null;
+	static SessionFactory sessionFactory = null;
 
-   public static SessionFactory getSessionFactory() {
-      if (sessionFactory != null) {
-         return sessionFactory;
-      }
-      Configuration configuration = new Configuration().configure();
-      StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-      sessionFactory = configuration.buildSessionFactory(builder.build());
-      return sessionFactory;
-   }
-   
-   public static List<Student> getAccount(String keyword) {
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory != null) {
+			return sessionFactory;
+		}
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties());
+		sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+
+	public static List<Student> getAccount(String keyword) {
 	   	  List<Student> resultList = new ArrayList<Student>();
 	      Session session = getSessionFactory().openSession();
 	      Transaction tx = null; 
-	      int nuid = Integer.valueOf(keyword);
 
 	      try {
+	    	 int nuid = Integer.valueOf(keyword);
 	         tx = session.beginTransaction();
 	         
 	         List<?> accounts = session.createQuery("FROM Student WHERE id="+Integer.toString(nuid)).list();
@@ -51,159 +52,165 @@ public class UtilDBLopezBanderas {
 	         
 	         tx.commit();
 	      } catch (HibernateException e) {
-	         if (tx != null)
-	            tx.rollback();
-	         e.printStackTrace();
-	      } finally {
+	    	  if (tx != null)
+	    		  tx.rollback();
+	    	  e.printStackTrace();
+	      } catch (java.lang.NumberFormatException e) {
+	    	  if (tx != null)
+			      tx.rollback();
+			  e.printStackTrace();
+	      }	finally {
 	         session.close();
 	      }
 	      return resultList;
 	   }
 
-   public static List<Course> listCourses() {
-      List<Course> resultList = new ArrayList<Course>();
+	public static List<Course> listCourses() {
+		List<Course> resultList = new ArrayList<Course>();
 
-      Session session = getSessionFactory().openSession();
-      Transaction tx = null;  // each process needs transaction and commit the changes in DB.
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null; // each process needs transaction and commit the changes in DB.
 
-      try {
-         tx = session.beginTransaction();
-         List<?> courses = session.createQuery("FROM Course").list();
-         for (Iterator<?> iterator = courses.iterator(); iterator.hasNext();) {
-            Course course = (Course) iterator.next();
-            resultList.add(course);
-         }
-         tx.commit();
-      } catch (HibernateException e) {
-         if (tx != null)
-            tx.rollback();
-         e.printStackTrace();
-      } finally {
-         session.close();
-      }
-      return resultList;
-   }
+		try {
+			tx = session.beginTransaction();
+			List<?> courses = session.createQuery("FROM Course").list();
+			for (Iterator<?> iterator = courses.iterator(); iterator.hasNext();) {
+				Course course = (Course) iterator.next();
+				resultList.add(course);
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
 
-   public static List<Course> listCourses(String keyword) {
-      List<Course> resultList = new ArrayList<Course>();
-      int nuid = Integer.valueOf(keyword);
+	public static List<Course> listCourses(String keyword) {
+		List<Course> resultList = new ArrayList<Course>();
+		int nuid = Integer.valueOf(keyword);
 
-      Session session = getSessionFactory().openSession();
-      Transaction tx = null;
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
 
-      try {
-         tx = session.beginTransaction();
-        // System.out.println((Course)session.get(Course.class, 1)); // use "get" to fetch data
-        // Query q = session.createQuery("FROM Employee");
-         List<?> courses = session.createQuery("FROM Course WHERE id="+Integer.toString(nuid)).list();
-         for (Iterator<?> iterator = courses.iterator(); iterator.hasNext();) {
-            Course course = (Course) iterator.next();
-            resultList.add(course);
-            
-         }
-         tx.commit();
-      } catch (HibernateException e) {
-         if (tx != null)
-            tx.rollback();
-         e.printStackTrace();
-      } finally {
-         session.close();
-      }
-      return resultList;
-   }
-   
-   public static List<Course> listCoursesByGrade(String nuid, String grade) {
-	      List<Course> resultList = new ArrayList<Course>();
+		try {
+			tx = session.beginTransaction();
+			// System.out.println((Course)session.get(Course.class, 1)); // use "get" to
+			// fetch data
+			// Query q = session.createQuery("FROM Employee");
+			List<?> courses = session.createQuery("FROM Course WHERE id=" + Integer.toString(nuid)).list();
+			for (Iterator<?> iterator = courses.iterator(); iterator.hasNext();) {
+				Course course = (Course) iterator.next();
+				resultList.add(course);
 
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
 
-	      try {
-	         tx = session.beginTransaction();
-	         List<?> courses = session.createCriteria(Course.class).add(Restrictions.eq("id", Integer.valueOf(nuid)))
-	        		 .add(Restrictions.eq("grade", grade)).list();
-	         for (Iterator<?> iterator = courses.iterator(); iterator.hasNext();) {
-	            Course course = (Course) iterator.next();
-	            resultList.add(course);
-	            
-	         }
-	         tx.commit();
-	      } catch (HibernateException e) {
-	         if (tx != null)
-	            tx.rollback();
-	         e.printStackTrace();
-	      } finally {
-	         session.close();
-	      }
-	      return resultList;
-	   }
+	public static List<Course> listCoursesByGrade(String nuid, String grade) {
+		List<Course> resultList = new ArrayList<Course>();
 
-   public static boolean addCourse(String id, String course_id, String section, String semester, String title, String credit_hours, String grade) {
-      Session session = getSessionFactory().openSession();
-      Transaction tx = null;
-      boolean result = true;
-      try {
-         tx = session.beginTransaction();
-         session.save(new Course(Integer.valueOf(id), course_id, section, semester, title, Integer.valueOf(credit_hours), grade));
-         tx.commit();
-      } catch (HibernateException e) {
-    	 result = false;
-         if (tx != null)
-            tx.rollback();
-         e.printStackTrace();
-      } finally {
-         session.close();
-      }
-      return result;
-   }
-   
-   public static boolean deleteCourse(String id, String course_id, String section, String semester) {
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
-	      boolean result = true;
-	      try {
-	         tx = session.beginTransaction();
-	         
-	         Course course = (Course) session.createCriteria(Course.class).add(Restrictions.eq("id", Integer.valueOf(id)))
-	        		 .add(Restrictions.eq("course_id", course_id))
-	        		 .add(Restrictions.eq("section", section)).uniqueResult();
-	         
-	         session.delete(course);
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
 
-	         tx.commit();
-	      } catch (HibernateException e) {
-	    	 result = false;
-	         if (tx != null)
-	            tx.rollback();
-	         e.printStackTrace();
-	      } catch(java.lang.IllegalArgumentException e) {
-	    	  result = false;
-		      if (tx != null)
-		         tx.rollback();
-		      e.printStackTrace();
-	      }
-	      finally {
-	         session.close();
-	      }
-	      return result;
-	   }
-   
-   public static boolean addStudent(String id, String first_name, String last_name) {
-	      Session session = getSessionFactory().openSession();
-	      Transaction tx = null;
-	      boolean result = true;
-	      try {
-	         tx = session.beginTransaction();
-	         session.save(new Student(Integer.valueOf(id), first_name, last_name));
-	         tx.commit();
-	      } catch (HibernateException e) {
-	    	 result = false;
-	         if (tx != null)
-	            tx.rollback();
-	         e.printStackTrace();
-	      } finally {
-	         session.close();
-	      }
-	      return result;
-	   }
+		try {
+			tx = session.beginTransaction();
+			List<?> courses = session.createCriteria(Course.class).add(Restrictions.eq("id", Integer.valueOf(nuid)))
+					.add(Restrictions.eq("grade", grade)).list();
+			for (Iterator<?> iterator = courses.iterator(); iterator.hasNext();) {
+				Course course = (Course) iterator.next();
+				resultList.add(course);
+
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return resultList;
+	}
+
+	public static boolean addCourse(String id, String course_id, String section, String semester, String title,
+			String credit_hours, String grade) {
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		boolean result = true;
+		try {
+			tx = session.beginTransaction();
+			session.save(new Course(Integer.valueOf(id), course_id, section, semester, title,
+					Integer.valueOf(credit_hours), grade));
+			tx.commit();
+		} catch (HibernateException e) {
+			result = false;
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	public static boolean deleteCourse(String id, String course_id, String section, String semester) {
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		boolean result = true;
+		try {
+			tx = session.beginTransaction();
+
+			Course course = (Course) session.createCriteria(Course.class)
+					.add(Restrictions.eq("id", Integer.valueOf(id))).add(Restrictions.eq("course_id", course_id))
+					.add(Restrictions.eq("section", section)).uniqueResult();
+
+			session.delete(course);
+
+			tx.commit();
+		} catch (HibernateException e) {
+			result = false;
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} catch (java.lang.IllegalArgumentException e) {
+			result = false;
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return result;
+	}
+
+	public static boolean addStudent(String id, String first_name, String last_name) {
+		Session session = getSessionFactory().openSession();
+		Transaction tx = null;
+		boolean result = true;
+		try {
+			tx = session.beginTransaction();
+			session.save(new Student(Integer.valueOf(id), first_name, last_name));
+			tx.commit();
+		} catch (HibernateException e) {
+			result = false;
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return result;
+	}
 }
